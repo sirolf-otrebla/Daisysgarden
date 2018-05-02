d0967d55ae132832f85e2db2a9a68129bf51fa1e
lucafalaschini <luca.falaschini@mail.polimi.it>
2018-05-02 21:23:40 +0200

    subnav responsive

---
+++ public/assets/js/script.js
@@ -0,0 +1,36 @@
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+
+    $(document).ready( function() {
+      console.log("ready");
+        /* Check width on page load*/
+        if ( isMobile.any() || $(document).width() < 544) {
+            $('#subnav').addClass('flex-column');
+        }
+    });
+
+    $(window).resize(function() {
+        /*If browser resized, check width again */
+        if (isMobile.any() || $(document).width() < 544) {
+            $('#subnav').addClass('flex-column');
+        }
+        else {$('#subnav').removeClass('flex-column');}
+    });
