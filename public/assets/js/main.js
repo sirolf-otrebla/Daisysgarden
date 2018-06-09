f76da2272ef2d2adf7dfffdc251ce713c4b3c08e
febbo <leonardo.febbo95@hotmail.it>
2018-06-09 18:35:11 +0200

    map people & fix images dimensions & fix onclick

---
+++ public/assets/js/main.js
@@ -0,0 +1,43 @@
+var toOutput = "";
+var iterator = 1;
+var iterator2 = 1;
+
+/* ----------------- NAV JS -----------------*/
+
+$(document).ready(function () {
+    $('#nav-menu').click(function () {
+        $('#nav-menu').toggleClass('open');
+        $('.nav-resp').toggleClass('open');
+    });
+    if (typeof init === "function") {
+        init();
+    }
+    //This variable must been set on the html page to call the APIs
+    if (apiCall != "") {
+        show(apiCall);
+    }
+});
+
+
+/* ----------------- API -----------------*/
+function show(what, callback) {
+    var levels = what.split("/");
+    var level1 = levels[0];
+    var level2 = levels[1];
+    var parameters = window.location.search.substr(1);
+
+    //console.log(what);
+
+    fetch("/api/" + what)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            //toOutput += '<div class="row form-group">';
+
+            data.map(format);
+
+            toOutput += '</div>';
+            $("#list").append(toOutput);
+        });
+}
