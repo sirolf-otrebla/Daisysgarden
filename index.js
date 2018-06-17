d1fc2c776020f27d240e0fc3d5b938019679a842
lucafalaschini <luca.falaschini@mail.polimi.it>
2018-06-17 18:52:40 +0200

    fix

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ let knex;
 let sqlDbFactory = require("knex");
 let dbManagement = require("./dbManagement");
 let queries = queryContainer.queries;
-const TEST = true;
-const SETUP = true;
+const TEST = false;
+const SETUP = false;
 
 function defineSQLenv(callback) {
     /* Locally we should launch the app with TEST=true to use SQLlite:
